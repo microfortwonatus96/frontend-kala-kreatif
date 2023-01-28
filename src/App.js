c33377f8195b5202c643101e6ffe0cc62105dd78
Micro Fortwonatus <microfortwonatus6@gmail.com>
2023-01-29 05:54:29 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import './App.css';
-import Login from './components/Login/Login';
-import Product from './components/Product/Product';
-import {BrowserRouter as Router, Routes, Route}  from 'react-router-dom'
-import DetailProduct from './components/Product/DetailProduct';
-import Header from './components/Header/Header';
-import Cart from './components/Cart/Cart';
-import Head from './components/Header/Head';
+// import './App.css';
+// import Login from './components/Login/Login';
+// import Product from './components/Product/Product';
+// import {BrowserRouter as Router, Routes, Route}  from 'react-router-dom'
+// import DetailProduct from './components/Product/DetailProduct';
+// import Header from './components/Header/Header';
+// import Cart from './components/Cart/Cart';
+// import Head from './components/Header/Head';
 
-function App() {
-  return (
-    <>
-    <div className='app'>
-    <Router>  
-      <Header />
-      {/* <Head /> */}
-      <Routes>
-        <Route exact path='/' element={<Product/>} /> 
-        <Route exact path='/cart' element={<Cart/>} /> 
-        <Route path='/login' element={<Login/>} />
-        <Route path='/detail-product/:id' element={<DetailProduct/>} />
-      </Routes>
+// function App() {
+//   return (
+//     <>
+//     <div className='app'>
+//     <Router>  
+//       <Header />
+//       {/* <Head /> */}
+//       <Routes>
+//         <Route exact path='/' element={<Product/>} /> 
+//         <Route exact path='/cart' element={<Cart/>} /> 
+//         <Route path='/login' element={<Login/>} />
+//         <Route path='/detail-product/:id' element={<DetailProduct/>} />
+//       </Routes>
     
-    </Router>
-    </div>
+//     </Router>
+//     </div>
 
-    </>
-  );
-}
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Context } from "./Context/Contex";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+// import Footer from './component/Footer/Footer';
+import Product from './components/Product/Product';
+import Header from './components/Header/Header';
+import Login from './components/Login/Login';
+import DetailProduct from './components/Product/DetailProduct';
+import { Context } from './Context/Contex';
+import Cart from './components/Cart/Cart';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Header />
+        <Outlet />
+        {/* <Footer /> */}
+      </>
+    ),
+    errorElement: <p>Page Not Found</p>,
+    children: [
+      {
+        path: "/",
+        element: <Product />,
+      },
+     
+      {
+        path: "/detail-product/:id",
+        element: <DetailProduct />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ]
+    ,
+    
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <Context>
-    <App />
-  </Context>
-  // </React.StrictMode>
+ <Context> <RouterProvider router={router} /></Context>
 );
 
 // If you want to start measuring performance in your app, pass a function
